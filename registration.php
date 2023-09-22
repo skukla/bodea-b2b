e663b54c5e0f531dd3a1f4dbe79faf972c091285
Steve Kukla <kukla@adobe.com>
2023-09-22 12:50:20 -0400

    Renamed module to kukla-bodea

--- registration.php
+++ registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'customdemo_bodea', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'kukla_bodea', __DIR__);
