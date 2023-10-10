a98134ee01236910f38e48f23b4dc56112153b72
Steve Kukla <kukla@adobe.com>
2023-10-10 13:42:11 -0400

    Updated data pack theme

--- media/theme/Custom/blank/registration.php
+++
@@ -1,3 +0,0 @@
-<?php
-use \Magento\Framework\Component\ComponentRegistrar;
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Custom/blank', __DIR__);
---
+++ media/theme/kukla/bodea/registration.php
@@ -0,0 +1,3 @@
+<?php
+use \Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/kukla/bodea', __DIR__);
